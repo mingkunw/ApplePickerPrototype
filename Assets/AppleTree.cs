8d036bdc9b40307570053448e50ffaca1af49c23
mingkunw <andymkwang@gmail.com>
2016-09-11 20:46:08 -0400

    Commit

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+	//Prefab for instantiating apples
+	public GameObject	applePrefab;
+
+	//Speed at which the AppleTree moves in meters/second
+	public float	speed = 10f;
+
+	//Distance where AppleTree turns around
+	public float	leftAndRightEdge = 20f;
+
+	//Chance that the AppleTree will change directions
+	public float	chanceToChangeDirections = 0.02f;
+
+	//Rate at which Apples will be instantiated
+	public float	secondsBetweenAppleDrops = 1f;
+	
+	void Start () {
+		//Dropping apples every second
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+
+	void Update () {
+		//Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		//Change Direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed);//Move right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);//Move left
+		}
+	}
+
+	void FixedUpdate(){
+		//Change Direction Randomly
+		if (Random.value < chanceToChangeDirections) {
+			speed *= -1; //Change direction
+		}
+	}
+
+	void DropApple(){
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
+	}
+}
